17fdf62e113dd32f35484a1c28c745602b776fba
tamueka <tamueka@hotmail.com>
2020-04-28 18:39:37 +0200

    ciclo de vida, hooks en vue

--- src/components/Pagina.vue
+++ src/components/Pagina.vue
@@ -2,19 +2,38 @@
   <section id="content">
     <h2 class="subheader">Pagina de pruebas</h2>
     <h3>{{ id_desde_url }}</h3>
+    <button @click="actualizarNombre()">Actualizar Nombre</button>
+    <h4>{{ nombre }}</h4>
   </section>
 </template>
 
 <script>
 export default {
   name: "Pagina",
+  created(){
+    console.log('Componente creado !!');
+  },
   mounted() {
     this.id_desde_url = this.$route.params.id;
+      console.log('Componente montado !!');
+  },
+  updated(){
+      console.log('Componente actualizado !!');
+  },
+  destroyed(){
+    console.log('Componente eliminado !!');
   },
   data() {
     return {
-      id_desde_url: null
+      id_desde_url: null,
+      nombre: "Samuel Ruiz"
     };
+  },
+  methods: {
+    actualizarNombre() {
+      this.nombre = "SAMUEL RUIZ";
+      console.log(this.nombre);
+    }
   }
 };
 </script>
