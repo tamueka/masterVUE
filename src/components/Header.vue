264a2587de80f3d8642898cb1b878a518e4a82ad
tamueka <tamueka@hotmail.com>
2020-04-28 16:38:05 +0200

    importamos nuestro archivo css en el app.vue en style con @import + creamos componentes y dentro de Header.vue modificamos el template

---
+++ src/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <header id="header">
+      <div class="center">
+        <!-- LOGO -->
+        <div id="logo">
+          <img src="../assets/logo.png" class="app-logo" alt="Logotipo" />
+          <span id="brand">
+            <strong>Curso</strong>Vue
+          </span>
+        </div>
+
+        <!-- MENU -->
+        <nav id="menu">
+          <ul>
+            <li>
+              <a href="index.html">Inicio</a>
+            </li>
+            <li>
+              <a href="blog.html">Blog</a>
+            </li>
+            <li>
+              <a href="formulario.html">Formulario</a>
+            </li>
+            <li>
+              <a href="#">Pagina 1</a>
+            </li>
+            <li>
+              <a href="#">Pagina 2</a>
+            </li>
+          </ul>
+        </nav>
+
+        <!--LIMPIAR FLOTADOS-->
+        <div class="clearfix"></div>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/MiComponente.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <hr>
+            <h1>Este es mi componente</h1>
+            <h2>{{ subtitulo }}</h2>
+        <hr>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MiComponente',
+    data(){
+        return{
+            subtitulo: 'Soy el subtitulo de mi componente'
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
