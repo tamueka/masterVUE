2c92e526d390ddd64cdeefefd853e939047de42c
tamueka <tamueka@hotmail.com>
2020-04-28 17:06:15 +0200

    creamos y separamos todo por componentes + cambiamos colores en style.css

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <template>
   <div id="app">
     <Header></Header>
+    <Slider></Slider>
+    <div class="center">
+      <LastArticles></LastArticles>
+      <Sidebar></Sidebar>
+      <div class="clearfix"></div>
+    </div>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
+import Header from './components/Header.vue';
+import Slider from './components/Slider.vue';
+import Sidebar from './components/Siderbar.vue';
+import LastArticles from './components/LastArticles.vue';
+import Footer from './components/Footer.vue';
+
 
 export default {
   name: 'App',
   components: {
     Header,
+    Slider,
+    Sidebar,
+    LastArticles,
+    Footer,
   }
 }
 </script>
