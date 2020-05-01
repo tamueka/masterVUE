2e1c75c307a9adb34562bd7cf7f7e551179e441e
tamueka <tamueka@hotmail.com>
2020-05-01 18:34:14 +0200

    comunicacion componente hijo -> padre (props)

---
+++ src/components/Pelicula.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <article class="article-item">
+        <h2>{{ pelicula.title }}</h2>
+      <div class="image-wrap">
+        <img :src="pelicula.image" :alt="pelicula.title" :title="pelicula.title" />
+      </div>
+      <span
+        class="date"
+        :class="{
+            yeargreen : pelicula.year >= 2015,
+            yearred: pelicula.year < 2015
+        }"
+      >{{ pelicula.year }}</span>
+      <a href="#">Leer más</a>
+
+      <div class="clearfix"></div>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Pelicula",
+  props: ['pelicula'],
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/components/Peliculas.vue
+++ src/components/Peliculas.vue
@@ -4,26 +4,21 @@
     <h2 class="subheader">Peliculas</h2>
     <!--Listado peliculas-->
     <div id="articles">
-      <article class="article-item" v-for="pelicula in peliculas" :key="pelicula.title">
-        <div class="image-wrap">
-          <img :src="pelicula.image" :alt="pelicula.title" :title="pelicula.title" />
-        </div>
-        <h2>{{ pelicula.title }}</h2>
-        <span class="date" :class="{
-            yeargreen : pelicula.year >= 2015,
-            yearred: pelicula.year < 2015
-        }">{{ pelicula.year }}</span>
-        <a href="#">Leer más</a>
-
-        <div class="clearfix"></div>
-      </article>
+      <div v-for="pelicula in peliculas" :key="pelicula.title">
+        <Pelicula :pelicula="pelicula"></Pelicula>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
+import Pelicula from "./Pelicula";
+
 export default {
   name: "Peliculas",
+  components: {
+    Pelicula
+  },
   data() {
     return {
       peliculas: [
@@ -44,11 +39,12 @@ export default {
           image:
             "https://pics.filmaffinity.com/the_lord_of_the_rings_the_fellowship_of_the_ring-952398002-large.jpg"
         }
-      ],
+      ]
     };
   }
 };
 </script>
 
 <style scoped>
+
 </style>
