6ef62f60c65f73d90e46451331baffb89e959d31
tamueka <tamueka@hotmail.com>
2020-05-04 17:19:56 +0200

    creamos formulario + lo vinculamos con un objeto + modficar datos y recibirlos en el componente

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -3,6 +3,36 @@
     <div class="center">
       <section id="content">
         <h2 class="subheader">Formulario</h2>
+        <form class="mid-form" @submit.prevent="mostrarUsuario()">
+          <div class="form-group">
+            <label for="nombre">Nombre</label>
+            <input type="text" name="nombre" v-model="user.nombre" />
+          </div>
+
+          <div class="form-group">
+            <label for="apellidos">Apellidos</label>
+            <input type="text" name="apellidos" v-model="user.apellidos"/>
+          </div>
+
+          <div class="form-group">
+            <label for="bio">Biografia</label>
+            <textarea name="bio" v-model="user.biografia"></textarea>
+          </div>
+
+          <div class="form-group radibuttons">
+            <input type="radio" name="genero" value="hombre" checked v-model="user.genero" /> Hombre
+            <input type="radio" name="genero" value="mujer" v-model="user.genero" /> Mujer
+            <input type="radio" name="genero" value="otro" v-model="user.genero" /> Otro
+          </div>
+
+          <div class="clearfix"></div>
+
+          <input type="submit" value="Enviar" class="btn btn-success" />
+        </form>
+        <div class="datos" v-if="user.nombre && user.apellidos">
+          <h2>Datos reactivos del formulario</h2>
+          <h4>{{user.nombre}} {{user.apellidos}}</h4>
+        </div>
       </section>
       <Sidebar></Sidebar>
       <div class="clearfix"></div>
@@ -17,6 +47,21 @@ export default {
   name: "Formulario",
   components: {
     Sidebar
+  },
+  data(){
+    return {
+      user:{
+        nombre: '',
+        apellidos: '',
+        biografia: '',
+        genero: ''
+      }
+    }
+  },
+  methods: {
+    mostrarUsuario(){
+      console.log(this.user)
+    }
   }
 };
 </script>
