77bbb03c276ada680e721a56c69af403f362418d
tamueka <tamueka@hotmail.com>
2020-05-08 19:10:10 +0200

    efecto cargando articulos en vue + fontawesome

--- src/components/Articles.vue
+++ src/components/Articles.vue
@@ -1,29 +1,35 @@
 <template>
-  <div id="article-list">
-    <article class="article-item" v-for="article in articles" :key="article._id">
-      <div class="image-wrap">
-        <img v-if="article.image" :src="url+'get-image/'+article.image" :alt="article.title" />
-        <img v-if="!article.image" src="../assets/images/default.jpg" :alt="article.title" />
-      </div>
-      <h2>{{ article.title }}</h2>
-      <span class="date">{{ article.date }}</span>
-      <a href="#">Leer más</a>
+  <section>
+    <div id="article-list" v-if="articles && articles.length >= 1">
+      <article class="article-item" v-for="article in articles" :key="article._id">
+        <div class="image-wrap">
+          <img v-if="article.image" :src="url+'get-image/'+article.image" :alt="article.title" />
+          <img v-if="!article.image" src="../assets/images/default.jpg" :alt="article.title" />
+        </div>
+        <h2>{{ article.title }}</h2>
+        <span class="date">{{ article.date }}</span>
+        <a href="#">Leer más</a>
 
-      <div class="clearfix"></div>
-    </article>
-  </div>
+        <div class="clearfix"></div>
+      </article>
+    </div>
+    <div v-else-if="articles && articles.length <= 1">
+      No hay articulos para mostrar
+    </div>
+    <div v-else><i class="fas fa-3x fa-sync fa-spin"></i></div>
+  </section>
 </template>
 
 <script>
-import Global from '../Global';
+import Global from "../Global";
 
 export default {
   name: "Articles",
   props: ["articles"],
-  data(){
-      return {
-         url: Global.url, 
-      }
+  data() {
+    return {
+      url: Global.url
+    };
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import Vuelidate from 'vuelidate'
 
+/* FONTAWESOME */
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
+
 /* RUTAS */
 import LastArticles from './components/LastArticles.vue'
 import Blog from "./components/Blog.vue";
@@ -11,7 +15,6 @@ import Pagina from "./components/Pagina.vue";
 import Error404 from "./components/Error404.vue";
 import MiComponente from './components/MiComponente.vue';
 import Peliculas from "./components/Peliculas.vue";
-
 
 Vue.config.productionTip = false
 
