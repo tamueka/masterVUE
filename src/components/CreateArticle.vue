d092c618612567175838414136e7f1a3c90629cb
tamueka <tamueka@hotmail.com>
2020-05-10 16:43:39 +0200

    creamos el componente formulario de creacion + vinculamos formulario con el objeto basado en el modelo + aplicamos estilos css

--- src/components/CreateArticle.vue
+++ src/components/CreateArticle.vue
@@ -1,10 +1,29 @@
 <template>
   <div class="general">
-    <Slider texto="BLOG"></Slider>
     <div class="center">
       <section id="content">
         <h1 class="subheader">Crear articulo</h1>
         <!-- Formulario -->
+        <form class="mid-form" @submit.prevent="save()">
+          <div class="form-group">
+            <label for="title">Titulo</label>
+            <input type="text" name="title" v-model="article.title"/>
+          </div>
+
+          <div class="form-group">
+            <label for="content">Contenido</label>
+            <textarea name="content" v-model="article.content"></textarea>
+          </div>
+
+          <div class="form-group">
+            <label for="image">Image</label>
+            <input type="file" name="image" />
+          </div>
+
+          <div class="clearfix"></div>
+
+          <input type="submit" value="Guardar" class="btn btn-success">
+        </form>
       </section>
       <Sidebar></Sidebar>
       <div class="clearfix"></div>
@@ -14,28 +33,30 @@
 
 <script>
 /* import axios from 'axios';
- */import Global from '../Global';
-import Slider from './Slider.vue';
-import Sidebar from './Sidebar.vue';
-import Article from '../models/Article';
+ */ import Global from "../Global";
+import Sidebar from "./Sidebar.vue";
+import Article from "../models/Article";
 /* import { required, minLength } from 'vuelidate/lib/validators';
  */
 export default {
   name: "CreateArticle",
   components: {
-      Slider,
-      Sidebar,
+    Sidebar
   },
   data() {
-      return {
-          url: Global.url,
-          article: new Article('', '', null, '')
-      }
+    return {
+      url: Global.url,
+      article: new Article("", "", null, "")
+    };
   },
   mounted() {
-      console.log(this.article)
+    console.log(this.article);
+  },
+  methods: {
+      save(){
+        console.log(this.article);  
+      }
   }
-
 };
 </script>
 
